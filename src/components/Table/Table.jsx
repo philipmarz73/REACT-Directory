0d40a981bb7f1b90c997ffd797d943658ad4351f
Phlipper <marzullop73@gmail.com>
2021-02-06 13:15:58 -0500

    set up ascending and descending sort functions and set to alternate them with click on 'Name' column head

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -4,7 +4,8 @@ import axios from "axios";
 const Table = () => {
     const [users, setUsers] = useState([]);
     const [usersToDisplay, setUsersToDisplay] = useState([]);
-    const [sortDirection, setSortDirection] = useState("asc");    
+    const [sortDirection, setSortDirection] = useState("asc");  
+
     useEffect(() => {
         axios.get ("https://randomuser.me/api/?results=50").then(response => {
             console.log(response.data);
@@ -14,6 +15,16 @@ const Table = () => {
     }, []);
     
     const sortByName = () => {
+            if (sortDirection === "asc") {
+                sortByNameAsc();
+                setSortDirection("desc");
+            }else{
+                sortByNameDesc();
+                setSortDirection("asc");
+            }
+    };
+    
+    const sortByNameAsc = () => {
         const tempUsers = [...users];
         const sortedUsers = tempUsers.sort((a,b) => {
             const aValue = a.name.first;
@@ -30,7 +41,21 @@ const Table = () => {
         setUsersToDisplay(sortedUsers);
        
     };
-    
+    const sortByNameDesc = () => {
+        const tempUsers = [...users];
+        const sortedUsers = tempUsers.sort((a,b) => {
+            const aValue = a.name.first;
+            const bValue = b.name.first;
+            if (aValue > bValue) {
+                return -1;
+            }
+            if (aValue < bValue) {
+                return 1;
+            }
+                return 0;
+        });
+            console.log(sortedUsers);
+        setUsersToDisplay(sortedUsers);
     return (
         <table className="table table-striped">
         <thead>
