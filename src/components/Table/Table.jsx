08da0fdca9aca086593f686f65f1a41dd3ac5606
Phlipper <marzullop73@gmail.com>
2021-02-06 13:55:48 -0500

    adding placeholder text for filter by phone number function and setting State

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -5,6 +5,7 @@ const Table = () => {
     const [users, setUsers] = useState([]);
     const [usersToDisplay, setUsersToDisplay] = useState([]);
     const [sortDirection, setSortDirection] = useState("asc");  
+    const [searchTerm, setSearchTerm] = useState("")
 
     useEffect(() => {
         axios.get ("https://randomuser.me/api/?results=50").then(response => {
@@ -59,34 +60,39 @@ const Table = () => {
     return (
         <div>
             <div>
-                <input type="text" placeholder="Enter phone number to filter"></input>
+                <input type="text" 
+                 placeholder="Enter phone number to filter"
+                 name="searchTerm" 
+                 value={searchTerm} 
+                 onChange={(e) => {
+                    setSearchTerm(e.target.value);
+                }}
+                ></input>
                 <button classnName="btn btn-primary">Search</button>
             </div>
-        </div>
-        
-        
-        <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Photo</th>
-            <th scope="col" onClick={sortByName}>Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Phone</th>
-            <th scope="col">DOB</th>
-          </tr>
-        </thead>
-        <tbody>
-          {usersToDisplay.map((user, index) => (
-          <tr key={index}>
-            <th scope="row">{user.id.value}</th>
-            <td>
-                <img src={user.picture.thumbnail} alt={user.name.first}></img>
-            </td>
-            <td>{user.name.first} {user.name.last}
-            </td>
-            <td>{user.email}
-            </td>
+            <div>
+              <table className="table table-striped">
+               <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Photo</th>
+                    <th scope="col" onClick={sortByName}>Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">DOB</th>
+                </tr>
+               </thead>
+               <tbody>
+                {usersToDisplay.map((user, index) => (
+                 <tr key={index}>
+                  <th scope="row">{user.id.value}</th>
+                  <td>
+                    <img src={user.picture.thumbnail} alt={user.name.first}></img>
+                  </td>
+                  <td>{user.name.first} {user.name.last}
+                  </td>
+                  <td>{user.email}
+                </td>
             <td>{user.phone}</td>
             <td>{user.dob.date}</td>
           </tr>
