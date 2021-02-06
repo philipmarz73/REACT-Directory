83f6d2cb1d3a311f6ab45326475ebd19f7e18c84
Phlipper <marzullop73@gmail.com>
2021-02-06 10:26:54 -0500

    added table headers for input in Table.jsx and imported Table into app.js

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 const Table = () => {
     return (
-        <table class="table">
+        <table className="table">
         <thead>
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">ID</th>
+            <th scope="col">Photo</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+            <th scope="col">DOB</th>
           </tr>
         </thead>
         <tbody>
